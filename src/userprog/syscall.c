e56aab5c473288bc74d4550dc644cd8cb8ea4469
Ralph Hermeling <ralphhermeling@gmail.com>
2025-08-08 12:04:53 +0000

    Validate user memory and clean up syscall_handler

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -1,7 +1,9 @@
 #include "userprog/syscall.h"
+#include <stdint.h>
 #include <stdio.h>
 #include <syscall-nr.h>
 #include "process.h"
+#include "string.h"
 #include "threads/interrupt.h"
 #include "threads/thread.h"
 #include "userprog/process.h"
@@ -16,8 +18,17 @@ static void syscall_handler(struct intr_frame*);
 
 void syscall_init(void) { intr_register_int(0x30, 3, INTR_ON, syscall_handler, "syscall"); }
 void syscall_exit(int status) {
-  printf("Exiting thread with status: %d", status);
-  thread_exit();
+  printf("%s: exit(%d)\n", thread_current()->pcb->process_name, status);
+  process_exit();
+}
+
+static unsigned syscall_write(int fd, void* buffer, unsigned size) {
+  /* Only support STDOUT for now */
+  ASSERT(fd == 1);
+
+  /* Write number of size character of buffer to STDOUT */
+  putbuf(buffer, size);
+  return size;
 }
 
 static bool validate_user_buffer(const void* buffer, size_t size) {
@@ -44,12 +55,29 @@ static bool get_user_args(void* esp, uint32_t* dest, size_t argc) {
   return true;
 }
 
-static void syscall_handler(struct intr_frame* f UNUSED) {
-  uint32_t args[MAX_ARGS];
+/*
+ * This does not check that the buffer consists of only mapped pages; it merely
+ * checks the buffer exists entirely below PHYS_BASE.
+ */
+static void validate_buffer_in_user_region(const void* buffer, size_t length) {
+  uintptr_t delta = PHYS_BASE - buffer;
+  if (!is_user_vaddr(buffer) || length > delta)
+    syscall_exit(-1);
+}
 
-  if (!get_user_args(f->esp, args, 1)) {
-    process_exit();
-  }
+/*
+ * This does not check that the string consists of only mapped pages; it merely
+ * checks the string exists entirely below PHYS_BASE.
+ */
+UNUSED static void validate_string_in_user_region(const char* string) {
+  uintptr_t delta = PHYS_BASE - (const void*)string;
+  if (!is_user_vaddr(string) || strnlen(string, delta) == delta)
+    syscall_exit(-1);
+}
+
+static void syscall_handler(struct intr_frame* f) {
+  uint32_t* args = f->esp;
+  validate_buffer_in_user_region(args, sizeof(uint32_t));
 
   /*
    * The following print statement, if uncommented, will print out the syscall
@@ -57,44 +85,20 @@ static void syscall_handler(struct intr_frame* f UNUSED) {
    * when debugging. It will cause tests to fail, however, so you should not
    * include it in your final submission.
    */
-
   /* printf("System call number: %d\n", args[0]); */
 
   if (args[0] == SYS_EXIT) {
-    if (!get_user_args(f->esp + NUM_BYTES_ARGUMENT_STACK, &args[1], 1)) {
-      printf("%s: exit(%d)\n", thread_current()->pcb->process_name, -1);
-    }
-
-    f->eax = args[1];
-    printf("%s: exit(%d)\n", thread_current()->pcb->process_name, args[1]);
-    process_exit();
+    validate_buffer_in_user_region(&args[1], sizeof(uint32_t));
+    syscall_exit((int)args[1]);
+    return;
   } else if (args[0] == SYS_WRITE) {
-    if (!get_user_args(f->esp + NUM_BYTES_ARGUMENT_STACK, &args[1], 3)) {
-      printf("%s: exit(%d)\n", thread_current()->pcb->process_name, -1);
-      f->eax = -1;
-      process_exit();
-    }
-    int fd = args[1];
-
-    const char* buffer = (const char*)args[2];
-    unsigned size = args[3];
-
-    /* Only support STDOUT for now */
-    ASSERT(fd == 1);
-
-    /* Write number of size character of buffer to STDOUT */
-    putbuf(buffer, size);
-    f->eax = size;
+    validate_buffer_in_user_region(&args[1], 3 * sizeof(uint32_t));
+    validate_buffer_in_user_region((void*)args[2], (unsigned)args[3]);
+    f->eax = syscall_write((int)args[1], (void*)args[2], (unsigned)args[3]);
     return;
   } else if (args[0] == SYS_PRACTICE) {
-    if (!get_user_args(f->esp + NUM_BYTES_ARGUMENT_STACK, &args[1], 1)) {
-      printf("%s: exit(%d)\n", thread_current()->pcb->process_name, -1);
-      f->eax = -1;
-      process_exit();
-    }
-    int i = args[1];
-
-    f->eax = i + 1;
+    validate_buffer_in_user_region(&args[1], sizeof(uint32_t));
+    f->eax = (int)args[1] + 1;
     return;
   }
 }
